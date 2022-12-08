1da2f26c3fc809725bcef0340b0e4236d2be0fa0
Nursultan Kurmanbekov <107362507+nurskurmanbekov@users.noreply.github.com>
2022-12-08 17:08:58 +0600

    Update index.js
    Co-Authored-By: MunarSarbashov <115638121+MunarSarbashov@users.noreply.github.com>
    Co-Authored-By: bekturali56 <75925385+bekturali56@users.noreply.github.com>
    Co-Authored-By: gitaktan <103026251+gitaktan@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -16,3 +16,15 @@ app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
+// Обработка ошибок, последний Middleware
+app.use(errorHandler)
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
